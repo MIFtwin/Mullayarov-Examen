1e52782ed4dd402f505db6b9bbd9705492f8e2b9
Ильгиз <the.mullayarov.twins@gmail.com>
2024-11-17 13:05:50 +0500

    Исправили код

--- .venv/Scripts/app.py
+++ .venv/Scripts/app.py
@@ -1,21 +1,21 @@
-from fastapi import FastAPI
-from datetime import date
-from typing import List
+import datetime
 from pydantic import BaseModel
+from fastapi import FastAPI
 
 class Order(BaseModel):
     number: int
-    startDate: date
+    startDate: datetime.date
     device: str
     problemType: str
     description: str
     client: str
     status: str
 
-repo: List[Order] = [
+
+repo = [
     Order(
         number=1,
-        startDate=date(2000, 12, 1),
+        startDate=datetime.date(2000, 12, 1),  
         device="123",
         problemType="123",
         description="123",
@@ -26,20 +26,7 @@ repo: List[Order] = [
 
 app = FastAPI()
 
-# Новый маршрут для корня
 @app.get("/")
 def read_root():
-    return repo  # Возвращаем содержимое репозитория
-
-# Существующий маршрут для получения всех заказов
-@app.get("/orders")
-def get_orders():
     return repo
-
-# Маршрут для добавления нового заказа
-@app.post("/orders")
-def create_order(order: Order):
-    repo.append(order)
-    return {"message": "Order created successfully", "order": order}
-
 
